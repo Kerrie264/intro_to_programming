527978c274d6e77838d9578e929e2cccebc32a28
Kerrie C <kerriemcd@gmail.com>
2018-10-17 06:36:09 +0100

    added exercises

--- cp2/name.rb
+++ cp2/name.rb
@@ -2,3 +2,15 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hello " + name
+
+# Question 3
+10.times do
+  puts name
+end
+
+# Question 4
+puts "What is your first name?"
+first = gets.chomp
+puts "What is your last name?"
+last = gets.chomp
+puts "Hello " + first + " " + last +"!"
