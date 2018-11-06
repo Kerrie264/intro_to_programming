85958e1f202b778d50e1a5236fbb04c149943627
Kerrie C <kerriemcd@gmail.com>
2018-11-06 13:04:01 +0000

    ruby

---
+++ CPBook/calc.rb
@@ -0,0 +1,42 @@
+puts 1+2
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+# Things to try
+puts 24*365 #Hours in a year
+puts 60*24*365*10 # Minutes in a decade
+puts 60*60*24*365*35 # Seconds old I am(ish)
+puts 1160000/365/60 #Autor age
+
+
+
+# Chapter 3
+# Letters
+puts 'Hello World'
+puts ''
+puts 'Good-bye'
+
+puts 'I like' + ' apple pie!'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
