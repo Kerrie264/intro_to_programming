a83024024f9797416cab42fa01cf62a514c0b7e6
Kerrie C <kerriemcd@gmail.com>
2018-11-07 07:32:00 +0000

    ruby

---
+++ CPBook/conversions.rb
@@ -0,0 +1,33 @@
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+
+# Things to try
+
+puts 'What is your first name, please? '
+first = gets.chomp
+puts 'What is your middle name, please? '
+middle = gets.chomp
+puts 'What is your last name, please? '
+last = gets.chomp
+puts 'Hello, ' + first + ' ' + middle + ' ' + last + ' ' +  'how are you?'
