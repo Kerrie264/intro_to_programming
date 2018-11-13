47ec75e0e219f3a4649d69c7e77f8bc9517bb74f
Kerrie C <kerriemcd@gmail.com>
2018-11-13 12:00:49 +0000

    ruby

---
+++ CPBook/arr+it.rb
@@ -0,0 +1,16 @@
+names = ['Ada', 'Belle', 'Chris']
+puts names
+puts 
+puts names[0]
+puts names[1]
+puts names[2]
+puts names[3]
+
+# The Method Each
+languages = ['English', 'Norwegian', 'Ruby']
+languages.each do |lang|
+  puts 'I love ' + lang + '!'
+  puts 'Don\'t you?'
+end
+puts 'And let\'s hear it for Java!'
+puts '<crickets chirp in the distance>'
