e3110dbdad4a6c041cd01e14ef396903b611b8a9
Kerrie C <kerriemcd@gmail.com>
2018-11-15 11:05:58 +0000

    ruby

--- CPBook/arr+it.rb
+++ CPBook/arr+it.rb
@@ -14,3 +14,61 @@ languages.each do |lang|
 end
 puts 'And let\'s hear it for Java!'
 puts '<crickets chirp in the distance>'
+
+# More Array Methods
+foods = ['artichoke', 'brioche', 'carmel']
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts 
+puts foods.join('  :)  ') + ' 8)'
+200.times do
+  puts []
+end
+
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'whiskers on kittens'
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+puts favorites.pop
+puts favorites
+puts favorites.length
+
+# A Few Things to Try
+inputs = []
+loop do 
+  puts 'Please give me a list of objects'
+  puts 'When your done press enter'
+  input = gets.chomp
+  if input == ''
+     break
+  end
+  inputs.push(input)
+end
+p inputs.sort
+
+# Table of Contents revisited
+book = ['Table of Contents',
+        'Chapter 1: Getting Started',
+        'Chapter 2: Numbers',
+        'Chapter 3: Letters',
+        ' page 1',
+        ' page 9',
+        ' page 13']
+
+
+line_width = 60
+puts (book[0].center(line_width))
+puts ''.center(line_width)
+#puts (book[1].ljust(line_width/2) + book[4].rjust(line_width/2))  
+#puts (book[2].ljust(line_width/2) + book[5].rjust(line_width/2))
+#puts (book[3].ljust(line_width/2) + book[6].rjust(line_width/2))
+book.each_with_index do |_, idx|
+  if idx > 0 && idx <= book.size/2
+    puts (book[idx].ljust(line_width/2) + book[idx + (book.size / 2)].rjust(line_width/2))  
+  end
+end
--- CPBook/looping.rb
+++ CPBook/looping.rb
@@ -1,123 +1,123 @@
-# input = ''
-# while input != 'bye'
-#   puts input
-#   input = gets.chomp
-# end
-# puts 'Come again soon!'
+input = ''
+while input != 'bye'
+  puts input
+  input = gets.chomp
+end
+puts 'Come again soon!'
 
-# while 'Spike' > 'Angel'
-#   input = gets.chomp
-#   puts input
-#   if input == 'bye'
-#     break
-#   end
-# end
-# puts 'Come again soon!'
+while 'Spike' > 'Angel'
+  input = gets.chomp
+  puts input
+  if input == 'bye'
+    break
+  end
+end
+puts 'Come again soon!'
 
-# # A little bit of logic
+# A little bit of logic
 
-# puts 'Hello, what\'s your name?'
-# name = gets.chomp
-# puts 'Hello, ' + name + '.'
-# if name == 'Chris'
-#   puts 'What a lovely name!'
-# else
-#   if name == 'Katy'
-#     puts 'What a lovely name!'
-#   end
-# end
-
-
-# puts 'Hello, what\'s your name?'
-# name = gets.chomp
-# puts 'Hello, ' + name + '.'
-# if name == 'Chris'
-#   puts 'What a lovely name!'
-# elsif name == 'Katy'
-#     puts 'What a lovely name!'
-# end
-
-# #DRY
-# puts 'Hello, what\'s your name?'
-# name = gets.chomp
-# puts 'Hello, ' + name + '.'
-# if name == 'Chris' || name == 'Katy'
-#   puts 'What a lovely name!'
-# end
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris'
+  puts 'What a lovely name!'
+else
+  if name == 'Katy'
+    puts 'What a lovely name!'
+  end
+end
 
 
-# i_am_chris = true
-# i_am_purple = false
-# i_like_beer = true
-# i_eat_rocks = false
-# puts i_am_chris && i_like_beer
-# puts i_like_beer && i_eat_rocks
-# puts i_am_purple && i_like_beer
-# puts i_am_purple && i_eat_rocks
-# puts
-# puts i_am_chris || i_like_beer
-# puts i_like_beer || i_eat_rocks
-# puts i_am_purple || i_like_beer
-# puts i_am_purple || i_eat_rocks
-# puts 
-# puts !i_am_purple
-# puts !i_am_chris
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris'
+  puts 'What a lovely name!'
+elsif name == 'Katy'
+    puts 'What a lovely name!'
+end
+
+#DRY
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Chris' || name == 'Katy'
+  puts 'What a lovely name!'
+end
 
 
-# while true
-#   puts 'What would you like to ask C to do?'
-#   request = gets.chomp
-#   puts 'You say, "C, please' + request + '".'
-#   puts 'C\'s response:'
-#   puts 'C ' + request + '".'
-#   puts '"Papa ' + request + ', too.'
-#   puts '"Mama ' + request + ', too.'
-#   puts '"Ruby ' + request + ', too.'
-#   puts '"Nono ' + request + ', too.'
-#   puts '"Emma ' + request + ', too.'
-#   puts 
-#   if request == 'stop'
-#     break
-#   end
-# end
+i_am_chris = true
+i_am_purple = false
+i_like_beer = true
+i_eat_rocks = false
+puts i_am_chris && i_like_beer
+puts i_like_beer && i_eat_rocks
+puts i_am_purple && i_like_beer
+puts i_am_purple && i_eat_rocks
+puts
+puts i_am_chris || i_like_beer
+puts i_like_beer || i_eat_rocks
+puts i_am_purple || i_like_beer
+puts i_am_purple || i_eat_rocks
+puts 
+puts !i_am_purple
+puts !i_am_chris
 
-# #A few things to try 
-# # 99 Bottles of Beer
-#   num = 99
-# while true
-#   puts '' + num.to_s + ' Bottles of Beer on the wall, ' + num.to_s + ' Bottles of Beer'
-#   puts 'Take one down and pass it around,' + (num-1).to_s + " Bottles of Beer on the wall\n\n"
-#   num = num - 1
-#   if num == 1
-#     break 
-#   end
-# end
-# puts "1 bottle of beer on the wall, 1 bottle of beer." + 
-#      " \nTake one down and pass it around, no more bottles of beer on the wall.\n\n"
-# puts 'No more Bottles of Beer on the wall, no more Bottles of Beer'
-# puts 'Go to the store, and buy some more, 99 Bottles of Beer on the wall'
 
-# # Deaf Grandma
-# count = 0
-# loop do
-#   talk = gets.chomp
-#   if talk != ''
-#     if talk == 'BYE'
-#       count += 1
-#       if count == 3
-#         break
-#       end
-#     else
-#       count = 0
-#     end
-#     if talk == talk.upcase 
+while true
+  puts 'What would you like to ask C to do?'
+  request = gets.chomp
+  puts 'You say, "C, please' + request + '".'
+  puts 'C\'s response:'
+  puts 'C ' + request + '".'
+  puts '"Papa ' + request + ', too.'
+  puts '"Mama ' + request + ', too.'
+  puts '"Ruby ' + request + ', too.'
+  puts '"Nono ' + request + ', too.'
+  puts '"Emma ' + request + ', too.'
+  puts 
+  if request == 'stop'
+    break
+  end
+end
 
-#       puts "NO, NOT SINCE  #{1930 + rand(20)}!"
-#     else
-#       puts 'HUH?! SPEAK UP, SONNY!'
-#     end
-#   end
-# end
+#A few things to try 
+# 99 Bottles of Beer
+  num = 99
+while true
+  puts '' + num.to_s + ' Bottles of Beer on the wall, ' + num.to_s + ' Bottles of Beer'
+  puts 'Take one down and pass it around,' + (num-1).to_s + " Bottles of Beer on the wall\n\n"
+  num = num - 1
+  if num == 1
+    break 
+  end
+end
+puts "1 bottle of beer on the wall, 1 bottle of beer." + 
+     " \nTake one down and pass it around, no more bottles of beer on the wall.\n\n"
+puts 'No more Bottles of Beer on the wall, no more Bottles of Beer'
+puts 'Go to the store, and buy some more, 99 Bottles of Beer on the wall'
+
+# Deaf Grandma
+count = 0
+loop do
+  talk = gets.chomp
+  if talk != ''
+    if talk == 'BYE'
+      count += 1
+      if count == 3
+        break
+      end
+    else
+      count = 0
+    end
+    if talk == talk.upcase 
+
+      puts "NO, NOT SINCE  #{1930 + rand(20)}!"
+    else
+      puts 'HUH?! SPEAK UP, SONNY!'
+    end
+  end
+end
 
 # Leap years
 puts 'Please enter a start year'
@@ -127,7 +127,12 @@ ending = gets.chomp.to_i
 puts 'The Leap Years are...'
 years = (start..ending).to_a
 years.each do |year|
-  if (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
+  #if (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
+  if year % 400 == 0
     puts year
+  elsif year % 100 == 0
+      next
+    elsif year % 4 == 0
+        puts year
   end 
 end
