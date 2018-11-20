c11322879a86966c9f3208bec921e467a6166126
Kerrie C <kerriemcd@gmail.com>
2018-11-20 12:34:24 +0000

    ruby

---
+++ CPBook/NCoO.rb
@@ -0,0 +1,16 @@
+alpha = Array.new + [12345]
+beta = String.new + 'hello'
+karma = Time.new
+puts "alpha = #{alpha}"
+puts "beta = #{beta}"
+puts "karma = #{karma}"
+
+
+time = Time.new
+time2 = time + 60
+puts time
+puts time2
+
+
+puts Time.local(2000, 1, 1)
+puts Time.local(1976, 8, 3, 13, 31)
